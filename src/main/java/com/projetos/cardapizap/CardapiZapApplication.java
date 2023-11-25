3468b032cce0eb0fadf10cb15c10bb0741d88343
jcr04 <joaocarlosreboucas15@gmail.com>
2023-11-25 18:04:09 -0300

    add project

---
+++ src/main/java/com/projetos/cardapizap/CardapiZapApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.cardapizap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapiZapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapiZapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetos/cardapizap/CardapiZapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.cardapizap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapiZapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
